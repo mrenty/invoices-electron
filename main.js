8b3a14b45610403c9e1c7f45829866868be2bc60
Michiel Renty <m.renty@gmail.com>
2017-01-07 12:18:15 +0100

    Added support for copy/paste

--- main.js
+++ main.js
@@ -114,6 +114,32 @@ function createMenu() {
             ]
         },
         {
+            label: 'Edit',
+            submenu: [
+                {
+                    role: 'undo'
+                },
+                {
+                    role: 'redo'
+                },
+                {
+                    type: 'separator'
+                },
+                {
+                    role: 'cut'
+                },
+                {
+                    role: 'copy'
+                },
+                {
+                    role: 'paste'
+                },
+                {
+                    role: 'selectall'
+                }
+            ]
+        },
+        {
             role: 'window',
             submenu: [
                 {
